dedd6307f7c470ef5d2c12f894e410e5a842ac2d
Saara Koskipaa <39309728+skoskipaa@users.noreply.github.com>
2019-05-30 20:02:03 +0300

    Lisätty käyttäjän rekisteröinti

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
 from application.auth.forms import LoginForm
+##
+from application.auth.forms import RegisterForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -25,5 +27,22 @@ def auth_logout():
     logout_user()
     return redirect(url_for("index"))
     
+###
+@app.route("/auth/registration", methods = ["GET"])
+def auth_getform():
+    return render_template("auth/registrationform.html", form = RegisterForm())
 
 
+@app.route("/auth/registration", methods = ["POST"])
+def auth_register():
+    form = RegisterForm(request.form)
+    name = form.name.data
+    username = form.username.data
+    password = form.password.data
+
+    u = User(name, username, password)
+
+    db.session().add(u)
+    db.session().commit()
+    return redirect(url_for("index"))
+##
